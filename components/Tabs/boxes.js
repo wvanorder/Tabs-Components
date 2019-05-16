59f824c957ecddc82bb60d77847b47f64332ea7b
VanOrder <vanorder@VanOrders-iMac.local>
2019-05-16 14:33:32 -0800

    carousel created

--- components/Tabs/boxes.js
+++ components/Tabs/boxes.js
@@ -1,14 +1,49 @@
 const buttonLeft = document.querySelector('.button-left')
 const buttonRight = document.querySelector(".button-right")
-const boxes = document.querySelectorAll('.box');
+const boxes = Array.from(document.querySelectorAll('.box'));
+var arrayQueue = 0;
+
+buttonRight.addEventListener('click', event => {
+    if(arrayQueue < boxes.length-1) {
+     arrayQueue +=1;
+    } else {
+        arrayQueue =0;
+    }
+ });
+
+buttonRight.addEventListener('click', event => 
+boxes.forEach(box => {
+    box.classList.add('box-hidden');
+}));
+
+buttonRight.addEventListener('click', event => {
+    boxes[arrayQueue].classList.remove('box-hidden');
+});
+
+
+buttonLeft.addEventListener('click', event => {
+    if(arrayQueue > 0) {
+        arrayQueue -=1;
+       } else {
+           arrayQueue = boxes.length-1;
+       }
+});
 
 
 buttonLeft.addEventListener('click', event => 
-Array.from(boxes).forEach(box => {
-    box.classList.toggle('box-hidden');
+boxes.forEach(box => {
+    box.classList.add('box-hidden');
 }));
 
-buttonRight.addEventListener('click', event => 
-Array.from(boxes).forEach(box => {
-    box.classList.toggle('box-hidden');
-}));
+buttonLeft.addEventListener('click', event => {
+    boxes[arrayQueue].classList.remove('box-hidden');
+});
+
+
+
+
+
+
+
+
+
